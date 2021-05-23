87c9cf5a6a7216ac3451f1c9652183c6647d3338
Jaleel Akbashev <adzhalil@gmail.com>
2021-05-23 22:39:14 +0200

    Changed a bit logic

--- Sources/CodableView/URLView.swift
+++ Sources/CodableView/URLView.swift
@@ -10,8 +10,9 @@ import Combine
 
 public struct URLView: Codable, View, Identifiable {
     
-    @State var contentView: CodableView = CodableView(type: .Text, value: CodableView.Value(text: "Error"))
-    private var cancellable: AnyCancellable?
+    @ObservedObject var viewModel: URLViewModel
+    
+    var cancellables = Set<AnyCancellable>()
 
     public var id = UUID()
     public var url: URL
@@ -31,55 +32,22 @@ public struct URLView: Codable, View, Identifiable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.id = try container.decode(UUID.self, forKey: .id)
         self.url = try container.decode(URL.self, forKey: .url)
-        cancellable = URLSession.shared
-            .dataTaskPublisher(for: url)
-            .tryMap() { element -> Data in
-                guard let httpResponse = element.response as? HTTPURLResponse,
-                    httpResponse.statusCode == 200 else {
-                        throw URLError(.badServerResponse)
-                    }
-                return element.data
-                }
-            .decode(type: CodableView.self, decoder: JSONDecoder())
-            .replaceError(with:
-                            CodableView(
-                                type: .VStack,
-                                subviews: [
-                                    CodableView(type: .Text, value: CodableView.Value(text: "Error"))
-                                ]
-                            )
-            )
-            .assign(to: \.contentView, on: self)
+        self.viewModel = URLViewModel(url: url)
+    }
+    
+    mutating func fetchView() {
+        
     }
     
     public init(url: URL) {
         self.url = url
-
-        self.cancellable = URLSession.shared
-            .dataTaskPublisher(for: url)
-            .tryMap() { element -> Data in
-                guard let httpResponse = element.response as? HTTPURLResponse,
-                    httpResponse.statusCode == 200 else {
-                        throw URLError(.badServerResponse)
-                    }
-                return element.data
-                }
-            .decode(type: CodableView.self, decoder: JSONDecoder())
-            .replaceError(with:
-                            CodableView(
-                                type: .VStack,
-                                subviews: [
-                                    CodableView(type: .Text, value: CodableView.Value(text: "Error"))
-                                ]
-                            )
-            )
-            .assign(to: \.contentView, on: self)
+        self.viewModel = URLViewModel(url: url)
     }
     
     
     public var body: some View {
-        if self.contentView.subviews?.first?.value?.text != "Error"  {
-            contentView
+        if self.viewModel.contentView.type != nil {
+            AnyView(self.viewModel.contentView)
         } else {
             VStack {
                 Text("Loading...")
@@ -87,3 +55,33 @@ public struct URLView: Codable, View, Identifiable {
         }
     }
 }
+
+class URLViewModel: ObservableObject {
+    
+    @Published private(set) var contentView: CodableView = CodableView()
+    private var cancellable: AnyCancellable?
+    
+    init(url: URL) {
+        self.cancellable = URLSession.shared
+            .dataTaskPublisher(for: url)
+            .tryMap() { element -> Data in
+                guard let httpResponse = element.response as? HTTPURLResponse,
+                      httpResponse.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                return element.data
+            }
+            .decode(type: CodableView.self, decoder: JSONDecoder())
+            .replaceError(with:
+                            CodableView(
+                                type: .VStack,
+                                subviews: [
+                                    CodableView(type: .Text, value: CodableView.Value(text: "Error"))
+                                ]
+                            )
+            )
+            .receive(on: RunLoop.main)
+            .assign(to: \.contentView, on: self)
+    }
+}
+
